2a278ad40c8045cae361dfd687ec8b38e718d07a
Hari10 <hariashok1998@gmail.com>
2023-02-07 21:32:53 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/TempController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author HariNarayanan K
+ */
+@RestController
+public class TempController {
+    
+    @GetMapping("/")
+    public String getValue(){
+        System.out.println("Hi Chennai");
+        return "Hi da";
+    }
+}
